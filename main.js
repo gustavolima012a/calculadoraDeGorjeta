0c9fa3c803f249c957bf5b1797a9816709522f8f
Gustavo Lima <gustavo.lima@eaportal.org>
2025-04-10 00:26:11 -0300

    criação do js

---
+++ main.js
@@ -0,0 +1,13 @@
+let bill = 0
+let tipPercent = 0
+let numberOfPeople = 0
+
+function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+
+    console.log(bill)
+}
+
+function receiveNumberOfPeopleValue(){
+    NumberOfPeople = document.querySelector("#people").ValueAsNumber
+}
